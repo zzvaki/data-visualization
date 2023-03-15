576a0cd50041887aee72852ecee226a678f6143d
zhangzheng <zzvaki@gmail.com>
2023-03-15 17:16:17 +0800

    base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  vite: {
-    // Vite config options
-    base: '/data-visualization/'
-  },
+  base: '/data-visualization/',
   title: '数据可视化',
   description: 'A VitePress Site',
   themeConfig: {
